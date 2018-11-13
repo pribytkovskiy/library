55855a371904b532f5c1448251a6ed7dc12258e5
Dmitriy <dr.trem86@gmail.com>
2018-11-13 09:59:42 +0200

    add folder model

--- Run.rb
+++ Run.rb
@@ -3,8 +3,6 @@ require './AutoLoad.rb'
 library = Library.new
 library.save
 
-p library
-
 puts "Best reader: #{library.often_take_book_reader}"
 puts "Most_popular_book: #{library.most_popular_book}"
 puts "Ordered one of the three most popular books: #{library.readers_three_most_popular_books}"
